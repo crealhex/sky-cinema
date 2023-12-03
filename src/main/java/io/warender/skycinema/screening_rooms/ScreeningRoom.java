2146ee9acd8019639e8d9a853e50db338cfe8840
Luis Enco <encoluis@hotmail.com>
2023-12-03 15:23:22 -0500

    Refactor status field to use ScreeningRoomStatus enum.

--- src/main/java/io/warender/skycinema/screening_rooms/ScreeningRoom.java
+++ src/main/java/io/warender/skycinema/screening_rooms/ScreeningRoom.java
@@ -2,10 +2,9 @@ package io.warender.skycinema.screening_rooms;
 
 import io.warender.skycinema.seats.Seat;
 import jakarta.persistence.*;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Set;
 
 @Getter
 @Setter
@@ -21,7 +20,8 @@ public final class ScreeningRoom {
   private int capacity;
 
   @Column(name = "status")
-  private String status;
+  @Enumerated(EnumType.STRING)
+  private ScreeningRoomStatus status;
 
   @Column(name = "seats_count")
   private int seatsCount;
---
+++ src/main/java/io/warender/skycinema/screening_rooms/ScreeningRoomStatus.java
@@ -0,0 +1,7 @@
+package io.warender.skycinema.screening_rooms;
+
+public enum ScreeningRoomStatus {
+  OPEN,
+  MAINTENANCE,
+  CLOSED,
+}
