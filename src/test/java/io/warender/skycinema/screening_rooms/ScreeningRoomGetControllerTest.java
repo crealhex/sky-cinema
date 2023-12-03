6b1d9204be48141cab4e5cdcd9461e09ae5a9364
Luis Enco <encoluis@hotmail.com>
2023-12-03 15:23:41 -0500

    Add ScreeningRoomGetControllerTest for GET endpoint

---
+++ src/test/java/io/warender/skycinema/screening_rooms/ScreeningRoomGetControllerTest.java
@@ -0,0 +1,52 @@
+package io.warender.skycinema.screening_rooms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.warender.skycinema.shared.ApiVersions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ScreeningRoomGetControllerTest {
+
+  private final static String SCREENING_ROOMS_URL = ApiVersions.ONE + "/backoffice/screening-rooms";
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Autowired
+  private ScreeningRoomStorage screeningRoomStorage;
+
+
+  @BeforeEach
+  void cleanUp() {
+    screeningRoomStorage.deleteAll();
+  }
+
+  @Test
+  void givenScreeningRoomId_thenReturnScreemingRoomEntity() {
+    var screeningRoom = new ScreeningRoom();
+    screeningRoom.setCapacity(100);
+    screeningRoom.setStatus(ScreeningRoomStatus.OPEN);
+    screeningRoomStorage.save(screeningRoom);
+
+    var response =
+        testRestTemplate.getForEntity(SCREENING_ROOMS_URL + "/1", ScreeningRoom.class);
+
+    assertEquals(response.getStatusCode(), HttpStatus.OK);
+    assertNotNull(response.getBody());
+  }
+
+  @Test
+  void givenScreeningRoomId_whenScreeningRoomNotFound_thenReturnNotFound() {
+    var response =
+        testRestTemplate.getForEntity(SCREENING_ROOMS_URL + "/1", ProblemDetail.class);
+
+    assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+  }
+}
