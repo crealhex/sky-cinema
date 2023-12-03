f1cdeaa8c8217f24ff8fb5c6bf69fe3a32ee558d
Luis Enco <encoluis@hotmail.com>
2023-12-03 00:15:53 -0500

    Remove unused UuidGenerator import from Language class.

--- src/main/java/io/warender/skycinema/movies/Language.java
+++ src/main/java/io/warender/skycinema/movies/Language.java
@@ -3,7 +3,6 @@ package io.warender.skycinema.movies;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.UuidGenerator;
 
 @Getter
 @Setter
