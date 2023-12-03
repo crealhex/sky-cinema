fe9863458afb1195159b75fbdf3e4c20fc5d258c
Luis Enco <encoluis@hotmail.com>
2023-12-03 15:21:36 -0500

    Refactor test to use var for response declaration.

--- src/test/java/io/warender/skycinema/coworkers/CoworkerPostControllerTest.java
+++ src/test/java/io/warender/skycinema/coworkers/CoworkerPostControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CoworkerPostControllerTest {
@@ -22,7 +21,7 @@ class CoworkerPostControllerTest {
     var COWORKER_URL = ApiVersions.ONE + "/backoffice/coworkers";
     var coworker = new CreateCoworkerRequest("email", "password", UserRole.ROLE_ADMIN);
     var httpEntity = new HttpEntity<>(coworker);
-    ResponseEntity<Coworker> response =
+    var response =
         testRestTemplate.postForEntity(COWORKER_URL, httpEntity, Coworker.class);
 
     assertEquals(response.getStatusCode(), HttpStatus.CREATED);
