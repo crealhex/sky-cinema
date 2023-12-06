91bc130373b4e9a95cefc309fd7d23f446ebaaf3
Luis Enco <encoluis@hotmail.com>
2023-12-05 22:59:12 -0500

    Add endpoint to fetch single movie session by ID

--- src/main/java/io/warender/skycinema/movie_sessions/MovieSessionGetController.java
+++ src/main/java/io/warender/skycinema/movie_sessions/MovieSessionGetController.java
@@ -1,10 +1,13 @@
 package io.warender.skycinema.movie_sessions;
 
 import io.warender.skycinema.shared.ApiVersions;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +19,13 @@ public final class MovieSessionGetController {
   @GetMapping(ApiVersions.ONE + "/backoffice/movie-sessions")
   public ResponseEntity<List<MovieSession>> getAllMovieSessions() {
     var movieSessions = movieSessionStorage.findAll();
+    return ResponseEntity.ok(movieSessions);
+  }
+
+  @GetMapping(ApiVersions.ONE + "/backoffice/movie-sessions/{movieSessionId}")
+  public ResponseEntity<MovieSession> getAllMovieSessions(@PathVariable UUID movieSessionId) {
+    var movieSessions =
+        movieSessionStorage.findById(movieSessionId).orElseThrow(EntityNotFoundException::new);
     return ResponseEntity.ok(movieSessions);
   }
 }
